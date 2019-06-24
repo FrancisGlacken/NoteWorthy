b584bc5c38b804d1e8485b0d7cdbe607b532380d
Francis <francisglacken@gmail.com>
2019-06-24 02:40:05 -0400

    Added a working delete button to note activity

--- app/src/main/java/com/deltorostudios/noteworthy/NoteDao.kt
+++ app/src/main/java/com/deltorostudios/noteworthy/NoteDao.kt
@@ -1,10 +1,8 @@
 package com.deltorostudios.noteworthy
 
+import android.os.FileObserver.DELETE
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 
 @Dao
 interface NoteDao {
@@ -17,6 +15,9 @@ interface NoteDao {
 
     @Update
     fun updateNote(vararg note: Note)
+
+    @Delete
+    fun deleteNote(note: Note)
 
     @Query("DELETE FROM note_table")
     fun deleteAll()
